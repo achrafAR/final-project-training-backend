9d955ebe79cf28b8c4332047bcfbb784d615b7e9
achrafAR <achraf.alrachini@gmail.com>
2023-06-08 03:31:54 +0300

    create function to delete order from cart

--- controllers/myBooking.controller.js
+++ controllers/myBooking.controller.js
@@ -145,9 +145,6 @@ const deleteOfferFromBooking = async (req, res) => {
             return res.status(404).json({ error: 'Booking not found' });
         }
 
-        const removedOffer = booking.offers.find(offer => offer.offerId === offerId);
-
-
         const offerIndex = booking.offers.findIndex(offer => offer.offerId == offerId);
         console.log(offerIndex)
         if (offerIndex === -1) {
@@ -155,9 +152,8 @@ const deleteOfferFromBooking = async (req, res) => {
         }
 
         booking.offers.splice(offerIndex, 1);
-        booking.finalPrice = booking.finalPrice - removedOffer.total_price;
-        await booking.save();
 
+        
         
 
         await booking.save();
